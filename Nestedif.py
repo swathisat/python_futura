f6bbd1fd1c562a592c93dcea30599c26c28b8d64
swathisat <89576949+swathisat@users.noreply.github.com>
2022-04-25 14:05:59 +0530

    first commit

---
+++ Nestedif.py
@@ -0,0 +1,15 @@
+choice=int(input("enter a no  from 1-7"))
+if choice==1:
+    print("monday")
+elif choice==2:
+    print("tuesday")
+elif choice ==3:
+    print("wed")
+elif choice==5:
+    print("thrusday")
+elif choice==6:
+    print("friday")
+else:
+    print("sat")
+
+
